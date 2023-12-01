34ddb76065059bc2107546cebec58ea94c94a9e7
Emmayordi <yordanosteklay424@gmail.com>
2023-12-01 11:33:17 +0100

    The last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,19 +1,16 @@
-# Importing necessary module
+
 import random
 
-# Assigning a random signed number to the variable number
 number = random.randint(-10000, 10000)
 
-# Printing the assigned number
-print(f'The string Last digit of {number} is {str(number)[-1]}', end=' ')
+last_digit = abs(number) % 10
+status = ""
 
-# Extracting the last digit of the number
-last_digit = int(str(number)[-1])
-
-# Checking whether the last digit is greater than 5, equal to 0, or less than 6 and not 0
 if last_digit > 5:
-    print('and is greater than 5')
+    status = "greater than 5"
 elif last_digit == 0:
-    print('and is 0')
+    status = "0"
 else:
-    print(f'and is less than 6 and not 0')
+    status = "less than 6 and not 0"
+
+print("Last digit of", number, "is", last_digit, "and is", status)
