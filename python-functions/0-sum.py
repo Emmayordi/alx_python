07ee494028143927035e8bbf04560dc2fc2ce409
Emmayordi <yordanosteklay424@gmail.com>
2023-12-01 14:17:52 +0100

    so difficalt

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,8 +1,19 @@
 def add(a, b):
     result = a + b
     return result
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
 
+# Case 1: add(1, 2)
+result_case1 = add(1, 2)
+print("Correct output - case: add(1, 2):", result_case1)
 
+# Case 2: add(100, -2)
+result_case2 = add(100, -2)
+print("Correct output - case: add(100, -2):", result_case2)
+
+# Case 3: add(-100, -2)
+result_case3 = add(-100, -2)
+print("Correct output - case: add(-100, -2):", result_case3)
+
+# Case 4: add(0, 0)
+result_case4 = add(0, 0)
+print("Correct output - case: add(0, 0):", result_case4)
