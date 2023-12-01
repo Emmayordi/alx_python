26e714dfae008e70ebcdac6791438789a1cd13cb
Emmayordi <yordanosteklay424@gmail.com>
2023-12-01 13:43:54 +0100

    Hexa to decimal

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,2 +1,3 @@
-for number in range(99):  # Loop through numbers from 0 to 98
-    print(f"Decimal: {number}, Hexadecimal: {hex(number)}")
+for num in range(99):  #  0 to 98
+    print("Decimal: {}, Hexadecimal: {}".format(num, hex(num)))
+
