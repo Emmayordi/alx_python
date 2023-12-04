5992f740862f8b71a0c4b00daae6424645882652
Emmayordi <yordanosteklay424@gmail.com>
2023-12-04 11:17:33 +0100

    new commit

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -10,17 +10,18 @@ def pow(a, b):
 
     result = 1
     while b > 0:
-       
+        # If exponent is odd, multiply result with base
         if b % 2 == 1:
             result *= a
-       
+        # Square the base and halve the exponent
         a *= a
         b //= 2
 
     return result
 
-print(pow(2, 2))     
-print(pow(98, 2))    
-print(pow(98, 0))    
-print(pow(100, -2))  
-print(pow(-4, 5))     
+# Example usage:
+print(pow(2, 2))     # Output: 4
+print(pow(98, 2))    # Output: 9604
+print(pow(98, 0))    # Output: 1
+print(pow(100, -2))  # Output: 0.0001
+print(pow(-4, 5))     # Output: -1024
