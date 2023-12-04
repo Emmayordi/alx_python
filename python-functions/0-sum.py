6ad5077c42f6aeee666bf49759a55031e961f054
Emmayordi <yordanosteklay424@gmail.com>
2023-12-04 11:04:16 +0100

    correction

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,4 +1,10 @@
 def add(a, b):
     # Simply add the two integers and return the result
-    result = 1 + 2
+    result = a + b
     return result
+
+if __name__ == "__main__":
+    print(add(1, 2))       # Output: 3
+    print(add(98, 0))      # Output: 98
+    print(add(100, -2))    # Output: 98
+
