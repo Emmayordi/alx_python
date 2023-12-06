e13d39a67aded48f1e375d3df63e27726ddd60a4
Emmayordi <yordanosteklay424@gmail.com>
2023-12-06 14:04:39 +0100

    corection

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,9 +1,3 @@
-def reverse_string(string):
-    # Using string slicing to reverse the string
-    reversed_str = string[::-1]
-    return reversed_str
-
-# Example usage:
-input_str = "Hello, World!"
-result = reverse_string(input_str)
-print(result)
+str = "Holberton School"
+print("{}".format(str) * 3)
+print(str[:9])
