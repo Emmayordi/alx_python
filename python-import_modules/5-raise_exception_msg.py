f8cb40b2b9e861c8dd17b7be2bfa935351adb7d6
Emmayordi <yordanosteklay424@gmail.com>
2023-12-06 15:21:21 +0100

    new

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -5,4 +5,8 @@ def raise_exception_msg(message=""):
 try:
     raise_exception_msg("Python is cool\nC is fun")
 except NameError as e:
-    print("Caught a name exception:", e)
+    lines = str(e).split('\n')
+    if len(lines) >= 2:
+        print(lines[1])
+    else:
+        print("No valid message found in the exception")
