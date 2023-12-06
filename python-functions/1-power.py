206363a7b4aacc9336ddefa444f94887b4c8e2d5
Emmayordi <yordanosteklay424@gmail.com>
2023-12-06 13:16:52 +0100

    corection

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,27 +1,2 @@
-def custom_pow(base, exponent):
-    
-    if exponent == 0:
-        return 1
-    
-    
-    if exponent < 0:
-        base = 1 / base
-        exponent = -exponent
-
-    result = 1
-    while exponent > 0:
-        
-        if exponent % 2 == 1:
-            result *= base
-        
-        base *= base
-        exponent //= 2
-
-    return result
-
-
-print(custom_pow(2, 2))     
-print(custom_pow(98, 2))    
-print(custom_pow(98, 0))    
-print(custom_pow(100, -2))  
-print(custom_pow(-4, 5))     
+def pow(a, b):
+    return(a ** b)
