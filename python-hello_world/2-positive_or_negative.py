6dda9831885c96d0781893d6464e36d22c1fe45f
Emmayordi <yordanosteklay424@gmail.com>
2023-12-06 13:32:41 +0100

    correction

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,18 +1,8 @@
-
 import random
-
-
 number = random.randint(-10, 10)
-
-
-print(f'The number is: {number}')
-
-
 if number > 0:
-    print('is positive')
+    print(number, 'is positive')
 elif number == 0:
-    print('is zero')
+    print(number, 'is zero')
 else:
-    print('is negative')
-
-
+    print(number, 'is negative')
