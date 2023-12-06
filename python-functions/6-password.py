a4904a57bfe0c2b5320e658d7d8cf9c0d08633a3
Emmayordi <yordanosteklay424@gmail.com>
2023-12-06 14:18:03 +0100

    new corection

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,6 +1,32 @@
-validate_password = __import__('6-password').validate_password
+def validate_password(password):
+    # Check the length of the password
+    if len(password) < 8:
+        return False
 
-print(validate_password("Password123"))
-print(validate_password("abc123"))
-print(validate_password("Password 123"))
-print(validate_password("password123"))
+    # Check for at least one uppercase letter, one lowercase letter, and one digit
+    has_upper = False
+    has_lower = False
+    has_digit = False
+
+    for char in password:
+        if char.isupper():
+            has_upper = True
+        elif char.islower():
+            has_lower = True
+        elif char.isdigit():
+            has_digit = True
+
+    if not (has_upper and has_lower and has_digit):
+        return False
+
+    # Check if the password contains spaces
+    if ' ' in password:
+        return False
+
+    # If all checks pass, return True
+    return True
+
+
+password = "SecureP@ss123"
+result = validate_password(password)
+print(result)
