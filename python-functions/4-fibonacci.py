f3e5a15f8107032cb7bd78eaef1e5bba0d6efb5c
Emmayordi <yordanosteklay424@gmail.com>
2023-12-06 14:04:09 +0100

    4-fibonacci.py

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,18 @@
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []  # Return an empty list for non-positive values of n
+
+    # Initialize the Fibonacci sequence with the first two numbers
+    sequence = [0, 1]
+
+    # Generate the Fibonacci sequence up to the nth number
+    while len(sequence) < n:
+        next_number = sequence[-1] + sequence[-2]
+        sequence.append(next_number)
+
+    return sequence[:n]  # Return the first n numbers of the sequence
+
+# Example usage:
+n = 10
+result = fibonacci_sequence(n)
+print(result)
