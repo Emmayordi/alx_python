da4188ec6ddf392b4d68fb00ce0dc86f824ddfc7
Emmayordi <yordanosteklay424@gmail.com>
2023-12-06 15:10:30 +0100

    new commit

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,19 @@
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Division by zero is not allowed")
+        return None
+    except Exception as e:
+        print("An error occurred:", e)
+        return None
+    else:
+        print("Inside result: {}".format(result))
+        return result
+    finally:
+        print("Inside finally clause")
+
+# Example usage:
+a = 10
+b = 2
+result = safe_print_division(a, b)
