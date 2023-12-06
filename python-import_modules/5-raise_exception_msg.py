0ca7365bab3a4ef238a1b5d21627005611af39c3
Emmayordi <yordanosteklay424@gmail.com>
2023-12-06 15:19:12 +0100

    new change

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,8 +1,8 @@
-def raise_exception_msg(message="c is fun"):
+def raise_exception_msg(message=""):
     raise NameError(message)
 
-
+# Example usage:
 try:
-    raise_exception_msg("python is cool")
+    raise_exception_msg("Python is cool\nC is fun")
 except NameError as e:
     print("Caught a name exception:", e)
