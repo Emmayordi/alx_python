4bb309258e2e12c41594d07a80d9551d844aee4c
Emmayordi <yordanosteklay424@gmail.com>
2023-12-13 11:36:17 +0100

    new commit

--- python-data_structures/0-no_c.py
+++
@@ -1,8 +0,0 @@
-def no_c(my_string):
-    arr = []
-    for i in my_string:
-        if i == "c" or i == "C":
-            arr.append('')
-        else:
-            arr.append(i)
-    return "".join(arr)
