fbceaeaa86dadece00b2cb1ddf1c90da10c5c1e5
Emmayordi <yordanosteklay424@gmail.com>
2023-12-14 01:24:53 +0100

    new commit

---
+++ python-data_structures/ 2-multiple_returns.py
@@ -0,0 +1,13 @@
+def multiple_returns(sentence):
+    # Check if the sentence is empty
+    if not sentence:
+        return (0, None)  # Return a tuple with length 0 and first character as None
+    
+    length = len(sentence)
+    first_char = sentence[0]  # Get the first character of the sentence
+    return (length, first_char)
+
+# Example usage:
+sentence = "At Holberton school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
