25786ae31ae4b19205ec7133438e38f6c067ea5d
Emmayordi <yordanosteklay424@gmail.com>
2023-12-19 18:22:07 +0100

    corection agan

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,14 @@
+"""
+Defines an empty class BaseGeometry.
+"""
+class BaseGeometry:
+    """
+    An empty class representing the base geometry.
+    """
+    pass
+
+# Example usage:
+if __name__ == "__main__":
+    # Create an instance of the BaseGeometry class
+    geometry_instance = BaseGeometry()
+
