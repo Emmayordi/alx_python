85d237314c15ba38ee2e2cb0bbdeddf9c763a4eb
Emmayordi <yordanosteklay424@gmail.com>
2023-12-19 15:19:35 +0100

    new

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python3
+
+"""
+0. My first square
+    Contains an empty class that defines a square
+
+"""
+
+
 class Square:
-    def __init__(self, size):
-        self.__size = size  # Private attribute
-
-    def get_size(self):
-        return self.__size
-
-    def set_size(self, new_size):
-        # Add validation logic here if needed
-        self.__size = new_size
+    """
+    An empty class
+    """
