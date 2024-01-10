4db8b1aae25a2f2b0aa6e59c135f8148f3235d0f
Emmayordi <yordanosteklay424@gmail.com>
2024-01-10 18:10:16 +0100

    Inventing

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,8 @@
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
-        combination = "{:02d}".format(tens_digit * 10 + ones_digit)
-        print(combination, end=", " if ones_digit < 9 else "\n", flush=True)
+# . Inventing is a combination of brains and materials. The more brains you use, the less material you need
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{}{}".format(i, j), end='')
+        if i < 8:
+            print(", ", end='')
+
+print("\n")
