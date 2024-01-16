3d88d16e62702aae9b147df1f20489d644590257
Emmayordi <yordanosteklay424@gmail.com>
2024-01-16 20:22:53 +0100

    4. Cities by states

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,20 +1,21 @@
 """
-Script that lists all states with a name starting with N (upper N)
-from the database
+Script that takes in an argument and displays all values
+safe from MySQL injections!
 """
 import MySQLdb
 from sys import argv
 
+# The code should not be executed when imported
 if __name__ == '__main__':
-    # make a connection to the database
+
+    
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
     
+   
     cur = db.cursor()
-
-    cur.execute("SELECT * FROM states WHERE name\
-                LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
 
     rows = cur.fetchall()
     for i in rows:
