8c9069b46676a0a75c035903dc677b1618523b27
Emmayordi <yordanosteklay424@gmail.com>
2024-02-12 12:05:19 +0100

    Is it a number

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,49 @@
+""""
+new script that starts a Flask 
+web application
+"""
+
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays "Hello HBNB!" when the root route is accessed.
+    """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Displays "HBNB" when the /hbnb route is accessed.
+    """
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    Displays "C ", followed by the value of the text variable.
+    Replace underscore _ symbols with a space.
+    """
+    return 'C {}'.format(escape(text.replace('_', ' ')))
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is_cool'):
+    """
+    Displays "Python ", followed by the value of the text variable.
+    Replace underscore _ symbols with a space. Default value of text is "is cool".
+    """
+    return 'Python {}'.format(escape(text.replace('_', ' ')))
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """
+    Displays "{} is a number" if n is an integer.
+    """
+    return '{} is a number'.format(n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
